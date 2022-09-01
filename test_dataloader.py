a54933811663ba8e5baf7cc475c3391de0973301
Enes Erciyes <m.eneserciyes@gmail.com>
2022-09-01 12:05:58 +0200

    memory profiling

--- test_dataloader.py
+++ test_dataloader.py
@@ -4,8 +4,9 @@ import numpy
 import torch
 from dataloader import DataLoader
 import time
+from memory_profiler import profile
 
-
+@profile
 def main(opt):
     # import pydevd_pycharm
     #
@@ -14,17 +15,17 @@ def main(opt):
     # )
     start = time.time()
     dataloader = DataLoader(None, opt, opt.dataset)
-    for i in range(1):
+    for i in range(5):
         print(f"##### BATCH {i} #####")
         batch = dataloader.get_batch_fm("train", opt.npred)
         inputs, actions, targets, ids, car_sizes = batch
-        print("Inputs 0 hash", torch.mean(inputs[0]))
-        print("Inputs 1 hash", torch.mean(inputs[1]))
-        print("Inputs 2 hash", torch.mean(inputs[2]))
-        print("Actions hash", torch.mean(actions))
-        print("Targets 0 hash", torch.mean(targets[0]))
-        print("Targets 1 hash", torch.mean(targets[1]))
-        print("Targets 2 hash", torch.mean(targets[2]))
+        # print("Inputs 0 hash", torch.mean(inputs[0]))
+        # print("Inputs 1 hash", torch.mean(inputs[1]))
+        # print("Inputs 2 hash", torch.mean(inputs[2]))
+        # print("Actions hash", torch.mean(actions))
+        # print("Targets 0 hash", torch.mean(targets[0]))
+        # print("Targets 1 hash", torch.mean(targets[1]))
+        # print("Targets 2 hash", torch.mean(targets[2]))
     print("Took:", time.time() - start)
 
 
@@ -35,3 +36,4 @@ if __name__ == "__main__":
     numpy.random.seed(opt.seed)
     torch.manual_seed(opt.seed)
     main(opt)
+
